ff088aa14701b2375e0a92a2f07f3676a74caae7
dritz087 <formandw25@uww.edu>
2015-02-05 16:25:26 -0800

    Version 1.1

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,21 +1,36 @@
+package lab1;
+
 import java.util.Scanner;
 
 
 public class GuessingGame {
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
-		
 		int num = (int)(Math.random()*10 + 1);
-		
 		System.out.println("Please guess an integer between 1 and 10");
 		int guess = s.nextInt();
+		int cont = 1;
 		
+		while(cont == 1){
+		String decide = null;
 		if(num == guess){
-			System.out.println("Congratulations, you have guess the number.");
+			System.out.println("Congratulations, you have guessed the number.");
+			cont = 0;
 		}
-		else
-			System.out.println("You have guessed the number incorrectly");
-	
-	
+		else{
+			System.out.println("You have guessed the number incorrectly.  Would you like to guess again?(Y/N)");
+			decide = s.next();
+		
+		String yes = "Y";
+		String no = "N";
+		if(decide.equalsIgnoreCase(no)){
+			cont =0;
+		}
+		else{
+			System.out.println("Please guess an integer between 1 and 10");
+			guess = s.nextInt();
+		}
+		}
+		}
 	}
 }
